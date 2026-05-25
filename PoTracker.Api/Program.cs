25cde27d476835b0942549e008b2c9eb3a10de92
BHAVYA <bhavya4578@gmail.com>
2026-05-26 02:23:27 +0530

    Modified Program.cs for cors issue

--- PoTracker.Api/Program.cs
+++ PoTracker.Api/Program.cs
@@ -16,10 +16,11 @@ builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(Assembly.Load("PoTracker.Application")));
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", p =>
-        p.AllowAnyOrigin()
-         .AllowAnyMethod()
-         .AllowAnyHeader());
+    options.AddPolicy("Frontend", policy =>
+        policy.WithOrigins("https://ptrkuic22c0ee1.z19.web.core.windows.net/",
+                            "http://localhost:5173/")
+              .AllowAnyMethod()
+              .AllowAnyHeader());
 });
 
 builder.Services.AddEndpointsApiExplorer();
@@ -27,14 +28,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors("Frontend");
+
 app.UseRouting();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseCors("AllowAll");
 app.MapControllers();
 app.MapGet("/health", () => Results.Ok("Healthy"));
 app.Run();
